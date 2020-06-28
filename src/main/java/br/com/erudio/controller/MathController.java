376cead5deae44283dd7c6ad5a4ec12c0c88fa79
marina neri <marina_neri@hotmail.com>
2020-06-28 15:59:17 -0300

    Exceptions

--- src/main/java/br/com/erudio/controller/MathController.java
+++ src/main/java/br/com/erudio/controller/MathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exception.UnsuportedMathOperationException;
+
 @RestController
 public class MathController {
 	
@@ -12,7 +14,7 @@ public class MathController {
 	public Double soma (@PathVariable("num1") String num1, @PathVariable("num2") String num2) throws Exception {
 		
 		if (!isNumerico(num1) || !isNumerico(num2)) {
-			throw new Exception();
+			throw new UnsuportedMathOperationException("Insira um valor numérico: ");
 		}
 		
 		Double soma =  convertToDouble(num1)+convertToDouble(num2);
